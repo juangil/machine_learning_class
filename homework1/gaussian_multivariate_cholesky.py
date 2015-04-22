d255cbac93bf091d24f6d782abc153015a96b323
juangil <juangilopez@gmail.com>
2015-04-22 17:02:30 -0500

    cholesky and numpy sampling

--- homework1/gaussian_multivariate_cholesky.py
+++ homework1/gaussian_multivariate_cholesky.py
@@ -8,8 +8,16 @@ mean = np.matrix('0;0')
 mean_array = np.array((0.0,0.0))
 covariance = np.matrix('0.5 0.25; 0.25 1.0')
 pi = np.pi
-x = np.random.multivariate_normal(mean_array,covariance,10000)
+x = np.random.multivariate_normal(mean_array,covariance,1000)
 x3 = np.zeros(len(x))
+
+x_unif = np.random.random_sample((1000, 2))
+L = np.linalg.cholesky(covariance)
+x_chol = np.zeros((len(x_unif),2))
+x_chol_out = np.zeros(len(x_chol))
+for i in range (0,len(x_unif)):
+    tmp = mean_array + np.transpose(L*np.transpose(np.matrix(x[i])))
+    x_chol[i] = tmp
 
 #print x
 def gaussian_eval(x):
@@ -22,36 +30,47 @@ def gaussian_eval(x):
     return ret
 
 def generate_data():
-    for i in range(0,len(x)):
-        x3[i] = gaussian_eval(x[i])
+    for i in range(0,len(x_chol)):
+        #tmp = np.array([x1[i], x2[i]])
+        x_chol_out[i] = gaussian_eval(x_chol[i])
 
 def mean_estim():
     msum = np.array((0.0,0.0));
     msumx = 0.0
     for i in range(0,len(x)):
-        msum += x[i]
-        msumx += x[i,0]
-    return msum/len(x)
+        #print msum, np.array((x1[i], x2[i]))
+        msum += x_chol[i]#np.array((x1[i], x2[i]))
+        #msumx += x1[i]
+    #print msum,msumx/len(x1)
+    return msum/len(x_chol)
 
-
+'''
 def cov_estim(mean_estim):
     msum = np.matrix('0.0 0.0; 0.0 0.0')
     for i in range(0,len(x)):
-        tmp_x = np.transpose(np.matrix(x[i]))
+        tmp_x = np.transpose(np.matrix((x1[i],x2[i])))
         tmp_mean = np.transpose(np.matrix(mean_estim))
         tmp1 = (tmp_x - tmp_mean)
         tmp_mat = tmp1 * np.transpose(tmp1)
-        msum = msum + tmp_mat
+        msum = msum + np.transpose(tmp1)
     return msum/len(x)
-
+'''
 
 generate_data()
+#print x[0:,0], x[0:,1], x3
 mean_hat = mean_estim()
-cov_hat = cov_estim(mean_hat)
-print mean_hat, cov_hat
+#cov_hat = cov_estim(mean_hat)
 
+print mean_hat, #cov_hat
+#print mean_array, covariance
 
+'''
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.plot_trisurf(x[0:,0], x[0:,1], x3, cmap=cm.jet, linewidth=0.2, vmin = -4.0, vmax = 4.0)
+plt.show()
+'''
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_trisurf(x_chol[0:,0], x_chol[0:,1], x_chol_out, cmap=cm.jet, linewidth=0.2, vmin = -4.0, vmax = 4.0)
 plt.show()
