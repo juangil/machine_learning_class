9b3b5292ad2fd7b3f00a0d053638014705a47ff5
juangil <juangilopez@gmail.com>
2015-05-19 15:00:44 -0500

    lab finished, pending report

--- homework3/linear_classification.py
+++ homework3/linear_classification.py
@@ -15,93 +15,21 @@ label = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
 label_biclass_1 = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 1}
 
 
-def msEstim(X,T):
-    return (np.linalg.inv(np.transpose(X)*X)) * (np.transpose(X)*T)
+# Some utility functions------------------------------------------------------
 
+    # Function soft max
 def softMax(z):
     nume = np.exp(z)
     denom = np.sum(nume)
     ret = nume / denom
     return ret
 
+    # Logistic function
 def logisFunc(a):
     ret = 1.0/(1.0 + np.exp(a))
     return ret
 
-def logRegEstim(X,T):
-    PHI = X
-    N = X.shape[0]
-    M = X.shape[1]
-    Wold = np.transpose(np.matrix(np.random.random_sample(M)))
-    Wnew = np.transpose(np.matrix(np.zeros(M)))
-    EPS = 1e-6
-    diff = np.linalg.norm(Wold - Wnew)    
-    while 1:        
-        yy = np.transpose(Wold) * np.transpose(PHI)
-        yy = np.transpose(logisFunc(yy))
-        R = np.zeros((N,N))
-        for i in range(0,N):
-            R[i,i] = yy[i,0] * (1 - yy[i,0])
-        
-        z = (PHI*Wold) - (np.linalg.inv(R)*(yy - T))
-        Wnew = np.linalg.inv((np.transpose(PHI) * (R * PHI))) * (np.transpose(PHI)*R*z)
-        diff = np.linalg.norm(Wold - Wnew)
-        print Wold
-        if diff < EPS:
-            break
-        else:
-            Wold = Wnew
-    return Wnew
-
-def gmEstim(X,T, nclass):
-    msize = X[0].shape[1]
-    U = np.zeros((nclass,msize))
-    for i in range(0,nclass):
-        msum = np.zeros(msize)
-        Ni = 0
-        for j in range(0, len(X[:,0])):
-            tmp = (X[j,:])*(T[j,i])
-            Ni += (T[j,i])
-            msum = msum + tmp        
-        msum = msum / Ni
-        #print msum
-        U[i,:] = msum
-    #print U
-    Sigma = np.zeros((msize,msize))
-    for i in range(0, nclass):
-        S = np.zeros((msize,msize))
-        for j in range(0, len(X[:,0])):
-            tmp = np.matrix((X[j,:])*(T[j,i]) - (U[i,:])*(T[j,i]))
-            tmp2 = np.transpose(tmp)
-            tmps = tmp2*tmp
-            S = S + tmps
-        Sigma = Sigma + S
-    #print Sigma
-    Pi_ml = np.zeros((1,nclass))
-    for i in range(0, nclass):
-        Ni = 0
-        for j in range(0, len(X[:,0])):
-            Ni = Ni + T[j,i]
-        Pi_ml[0,i] = Ni / len(X[:,0])
-    
-    
-    Sigma = np.matrix(Sigma[1:, 1:])    
-    #print np.linalg.inv(Sigma)*np.transpose(mio)
-    Wgm = np.zeros((nclass, msize))
-    for i in range(0, nclass):
-        uk = np.transpose(np.matrix(U[i,1:]))
-        wi = np.linalg.inv(Sigma)*uk
-        w0 = ((-0.5*np.transpose(uk))*(np.linalg.inv(Sigma)*uk)) + np.log(Pi_ml[0,i])
-        #print w0, wi
-        tmp = np.zeros((1, msize))
-        tmp[0,0] = w0
-        tmp[0,1:] = np.transpose(wi)
-        wk = tmp
-        #print wk
-        Wgm[i,:] = wk
-    Wgm = np.matrix(Wgm)   
-    return Wgm
-
+    # Function to read the input data
 def assignVar(N):
     a = np.zeros((N,1))
     b = np.zeros((N,1))
@@ -123,6 +51,13 @@ def assignVar(N):
     XX = np.concatenate((unos,a,b,c,d),1)
     return np.matrix(XX), np.matrix(e)
 
+    # Mean and standard deviation evaluation
+def meanDesvCacl(z):
+    mmean = np.sum(z)/len(z)
+    desv = np.sum((z - mmean)*(z - mmean))/len(z)
+    return mmean, desv
+
+    # Extracting the class wich probability is the highest
 def extractMax(Yestim, nclass):
     for i in range(len(Yestim[:,0])):
         mayor = -1000000.0
@@ -139,6 +74,7 @@ def extractMax(Yestim, nclass):
                 Yestim[i,j] = 0.
     return Yestim
 
+    # Evaluationg accuracy of a preditcion
 def evalAccuracy(Yestim, Tvalid, nclass):
     true_positives = np.array([0.,0.,0.])
     true_negatives = np.array([0.,0.,0.])
@@ -158,12 +94,111 @@ def evalAccuracy(Yestim, Tvalid, nclass):
     ret = np.sum(true_positives + true_negatives)/np.sum(true_positives + false_positives + false_negatives + true_negatives)
     return ret
 
+
+# Several learners------------------------------------------------------------
+
+    # Least squares
+def msEstim(X,T):
+    return (np.linalg.inv(np.transpose(X)*X)) * (np.transpose(X)*T)
+
+    # Generative models
+def gmEstim(X,T, nclass):
+    msize = X[0].shape[1]
+    U = np.zeros((nclass,msize))
+    for i in range(0,nclass):
+        msum = np.zeros(msize)
+        Ni = 0
+        for j in range(0, len(X[:,0])):
+            tmp = (X[j,:])*(T[j,i])
+            Ni += (T[j,i])
+            msum = msum + tmp
+        msum = msum / Ni
+        #print msum
+        U[i,:] = msum
+    #print U
+    Sigma = np.zeros((msize,msize))
+    for i in range(0, nclass):
+        S = np.zeros((msize,msize))
+        for j in range(0, len(X[:,0])):
+            tmp = np.matrix((X[j,:])*(T[j,i]) - (U[i,:])*(T[j,i]))
+            tmp2 = np.transpose(tmp)
+            tmps = tmp2*tmp
+            S = S + tmps
+        Sigma = Sigma + S
+    #print Sigma
+    Pi_ml = np.zeros((1,nclass))
+    for i in range(0, nclass):
+        Ni = 0
+        for j in range(0, len(X[:,0])):
+            Ni = Ni + T[j,i]
+        Pi_ml[0,i] = Ni / len(X[:,0])
+
+
+    Sigma = np.matrix(Sigma[1:, 1:])
+    #print np.linalg.inv(Sigma)*np.transpose(mio)
+    Wgm = np.zeros((nclass, msize))
+    for i in range(0, nclass):
+        uk = np.transpose(np.matrix(U[i,1:]))
+        wi = np.linalg.inv(Sigma)*uk
+        w0 = ((-0.5*np.transpose(uk))*(np.linalg.inv(Sigma)*uk)) + np.log(Pi_ml[0,i])
+        #print w0, wi
+        tmp = np.zeros((1, msize))
+        tmp[0,0] = w0
+        tmp[0,1:] = np.transpose(wi)
+        wk = tmp
+        #print wk
+        Wgm[i,:] = wk
+    Wgm = np.matrix(Wgm)
+    return Wgm
+
+
+    # Logistic regression with Newton Raphson optimization
+def logRegEstim(X,T, Xtest):
+    PHI = X
+    N = X.shape[0]
+    M = X.shape[1]
+    #Wold = np.transpose(np.matrix(np.random.random_sample(M)))
+    Wold = np.transpose(np.matrix(np.zeros(M))) + 0.001
+    Wnew = np.transpose(np.matrix(np.zeros(M)))
+    EPS = 1e-6
+    diff = np.linalg.norm(Wold - Wnew)
+    cont = 0
+    Yestim = T
+    #print T
+    while cont < 10:
+        yy = np.transpose(Wold) * np.transpose(PHI)
+        yy = np.transpose(logisFunc(yy))
+        yy = 1.0 - yy
+        Yestim = yy
+        #print yy
+        #print ' '
+        R = np.zeros((N,N))
+        for i in range(0,N):
+            #print yy[i,0]
+            R[i,i] = (yy[i,0] * (1 - yy[i,0])) + 0.001
+
+        z = (PHI*Wold) - (np.linalg.inv(R)*(yy - T))
+        Wnew = np.linalg.inv((np.transpose(PHI) * (R * PHI))) * (np.transpose(PHI)*R*z)
+        diff = np.linalg.norm(Wold - Wnew)
+        if diff < EPS:
+            break
+        else:
+            Wold = Wnew
+        cont += 1
+
+    #print Xtest.shape
+    ret = np.transpose(Wnew) * np.transpose(Xtest)
+    ret = np.transpose(logisFunc(ret))
+    ret = 1.0 - ret
+    #print ret.shape
+    return ret
+
+# Cross validation -----------------------------------------------------------
 def evaluate(Xtrain, Xtest, Ttrain, Ttest, nclass):
     # Least squares estimation
     Wmse = msEstim(Xtrain,Ttrain)
     Yestim = np.transpose(Wmse)*np.transpose(Xtest)
     Yestim = np.transpose(Yestim)
-    #print Yestim
     Yestim = extractMax(Yestim, nclass)
     lsq_err = evalAccuracy(Yestim, Ttest, nclass)
 
@@ -177,19 +212,85 @@ def evaluate(Xtrain, Xtest, Ttrain, Ttest, nclass):
     gme_err = evalAccuracy(Yestim_gme, Ttest, nclass)
 
     # Discriminative model classification(logistic regression)
+    Yestim_log_reg = np.zeros((len(Ttest[:,0]), nclass))
+    Yestim_log_reg = np.matrix(Yestim_log_reg)
     for i in range(0, nclass):
-        logRegEstim(Xtrain, Ttrain[:,i])
+        Yestim_log_reg[:,i] = logRegEstim(Xtrain, Ttrain[:,i], Xtest)
+    Yestim_log_reg = extractMax(Yestim_log_reg,nclass)
+    #print Yestim_log_reg, Ttest
+    log_reg_err = evalAccuracy(Yestim_log_reg, Ttest, nclass)
+
+    return lsq_err, gme_err, log_reg_err
+
+def crossValidation(X, T, nclass, valid_num = 5):
+    N = len(X[:, 0])
+    Nvalid = (N/valid_num)
+    Ntest = N - Nvalid
+    index = np.random.permutation(N)
+    l = 0
+    r = Nvalid
+    err_lse_arr = np.zeros(valid_num)
+    err_gme_arr = np.zeros(valid_num)
+    err_log_arr = np.zeros(valid_num)
+    for i in range(0, valid_num):
+
+        Xtrain = []
+        Ttrain = []
+        if l == 0:
+            Xtrain = X[index[r:N], :]
+            Ttrain = T[index[r:N], :]
+        else:
+            if(r < N):
+                Xtrain = np.concatenate((X[index[0:l], :],X[index[r: N], :]))
+                Ttrain = np.concatenate((X[index[0:l], :],X[index[r: N], :]))
+            else:
+                Xtrain = X[index[0:l], :]
+                Ttrain = T[index[0:l], :]
 
 
-    print lsq_err, gme_err
+        Xvalid = X[index[l:r], :]
+        Tvalid = T[index[l:r], :]
+
+        l = r
+        r += Nvalid
+        '''
+        print Xvalid.shape
+        print Tvalid.shape
+        print Xtrain.shape
+        print Ttrain.shape
+        print ' '
+        '''
+        err_lse_arr[i], err_gme_arr[i], err_log_arr[i] = evaluate(Xtrain, Xvalid, Ttrain, Tvalid, nclass)
+
+    print 'Accuracy least squares classification: '
+    #print err_lse_arr
+    acc, desv = meanDesvCacl(err_lse_arr)
+    print 'Accuracy: ', acc, ' Standard Deviation', desv, ' Maximum accuracy: ', np.amax(err_lse_arr)
+    print '---------------------------------------------------------------'
+
+    print 'Accuracy generative models classification: '
+    #print err_gme_arr
+    acc, desv = meanDesvCacl(err_gme_arr)
+    print 'Accuracy: ', acc, ' Standard Deviation', desv, ' Maximum accuracy: ', np.amax(err_gme_arr)
+    print '---------------------------------------------------------------'
+
+    print 'Accuracy logistic regression classification: '
+    #print err_log_arr
+    acc, desv = meanDesvCacl(err_log_arr)
+    print 'Accuracy: ', acc, ' Standard Deviation', desv, ' Maximum accuracy: ', np.amax(err_log_arr)
+    print '---------------------------------------------------------------'
+
 
 
 X, T = assignVar(len(data[:,1]))
-evaluate(X,X,T,T, 3)
+#evaluate(X,X,T,T, 3)
+crossValidation(X,T,3)
 
+x1 = X[:,1]
+x2 = X[:,2]
+x3 = X[:,3]
+x4 = X[:,4]
 
-
-'''
 plt.figure(1)
 plt.subplot(231)
 plt.plot(x1[0:49], x2[0:49], 'bo')
@@ -217,4 +318,3 @@ plt.plot(x3[0:49], x4[0:49], 'bo')
 plt.plot(x3[50:99], x4[50:99], 'rx')
 plt.plot(x3[100:149], x4[100:149], 'g*')
 plt.show()
-'''
