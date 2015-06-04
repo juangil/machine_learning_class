7be7bfcd0c7e1a7c9ab96664e5d7172a4880bc1d
juangil <juangilopez@gmail.com>
2015-06-04 15:50:36 -0500

    pending bugs fto fix

--- end_project/2layer_neural_network.py
+++ end_project/2layer_neural_network.py
@@ -20,6 +20,9 @@ class nn:
         self.outs = K
         self.w1 = np.transpose(np.matrix(np.random.standard_normal((d+1)*M)))
         self.w2 = np.transpose(np.matrix(np.random.standard_normal(M*K)))
+        self.gradw1 = np.transpose(np.matrix(np.zeros((d+1)*M)))
+        self.gradw2 = np.transpose(np.matrix(np.zeros(M*K)))
+        self.W = np.concatenate((self.w1, self.w2), axis = 0)
         self.bas_fun = b
         self.ai = np.zeros((M, 1))
         self.zi = np.zeros((M, 1))
@@ -64,10 +67,6 @@ class nn:
 
     def computeDelta1(self, Y, T):
         tmp = T - Y
-        #print tmp[:,0], np.sum(tmp[:,0])
-        #print tmp
-        #print self.wmat2
-        #print np.transpose(tmp[:,0])*self.wmat2
         tmp = np.transpose(tmp)*self.wmat2
         h_der = 0
         if(self.bas_fun == 'sig'):
@@ -76,13 +75,10 @@ class nn:
             h_der = np.multiply(tmp, tmp2)
         elif(self.bas_fun == 'tanh'):
             h_der = (np.exp(self.ai) - np.exp(-self.ai))/(np.exp(self.ai) + np.exp(-self.ai))
-            h_der = 1 - np.square(h_der)
-        #print h_der.shape, tmp.shape
+            h_der = 1.0 - np.square(h_der)
         return np.multiply(h_der,tmp)
 
     def forwardPropagation(self, xx):
-        #print self.w1
-        #print self.w2
         self.weight1ToMat()
         self.weight2ToMat()
         A = self.activ1Eval(xx)
@@ -99,54 +95,69 @@ class nn:
         derivative2 = delta_2*np.transpose(self.zi)
         delta_1 = self.computeDelta1(Y,T)
         derivative1 = delta_1*np.transpose(xx)
-        #print derivative1
-        #print derivative2
-        #print self.w1
-        #print self.w2
         l = 0
         r = self.neurons
         for i in range(0, self.dim):
-            self.w1[l:r,0] = derivative1[:,i]
+            self.gradw1[l:r,0] = derivative1[:,i]
             l = r
             r = r + self.neurons
         l = 0
         r = self.outs
         for i in range(0, self.neurons):
-            self.w2[l:r,0] = derivative2[:,i]
+            self.gradw2[l:r,0] = derivative2[:,i]
             l = r
             r = r + self.outs
-        #print self.w1
-        #print self.w2
 
-        '''
-        derivative1 = delta_1*np.transpose(xx)
-        print derivative1
-        print self.wmat1
-        print ' '
-        print derivative2
-        print self.wmat2
-        '''
-
+        return np.concatenate((self.gradw1, self.gradw2), axis = 0)
 
     def process(self, xx, T):
-        Y = self.forwardPropagation(xx)
-        self.errorGradient(xx, Y, T)
-        return Y
+        cont = 0
+        while(cont < 50):
+            Y = self.forwardPropagation(xx)
+            gradient_w = self.errorGradient(xx, Y, T)
+            w_new = self.W - (0.5*gradient_w)
+            self.W = w_new
+            self.w1 = w_new[0:self.dim*self.neurons, 0]
+            self.w2 = w_new[self.dim*self.neurons:w_new.shape[0], 0]
+            print Y
+            cont = cont + 1
 
     def debug(self):
         print self.w2
         print self.wmat2
 
-
+'''
 X = np.random.standard_normal(size = (2,6))*10.
 T = np.random.binomial(1, 0.5, 6)
 tmp = np.ones(X.shape[1])
-mneural = nn(X.shape[0], 5, 4)
+mneural = nn(X.shape[0], 5, 1)
 XX = np.zeros((X.shape[0] + 1, X.shape[1]))
 XX[1:,:] = X
 XX[0,:] = tmp
 XX = np.matrix(XX)
-mneural.process(XX, T)
+print XX, T
+mneural.process(XX, T
+'''
+
+mdata = scipy.io.loadmat('ejemplo_class_uno.mat')
+X = np.array(mdata['X'])
+t = np.array(mdata['t'])
+for i in range(0, t.shape[0]):
+    if(t[i,0] == -1):
+        t[i,0] = 0
+t = np.matrix(t)
+t = np.transpose(t)
+tmp = np.ones(X.shape[0])
+XX = np.zeros((X.shape[0], X.shape[1] + 1))
+XX[:,1:] = X
+XX[:,0] = tmp
+XX = np.matrix(XX)
+XX = np.transpose(XX)
+mneural = nn(XX.shape[0] - 1, 2, 1)
+#print XX, t
+mneural.process(XX, t)
+
+
 
 '''
 out = np.zeros(1000)
