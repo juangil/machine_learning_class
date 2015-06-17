945853a6be171b14e4f5676702e5f90e8c2a88c9
juangil <juangilopez@gmail.com>
2015-06-17 18:33:13 -0500

    bugs with cost function fixed

--- end_project/2layer_neural_network.py
+++ end_project/2layer_neural_network.py
@@ -7,15 +7,27 @@ from pylab import *
 from matplotlib import cm
 import scipy
 
+def normalize_arr_gauss(x):
+    msum = np.sum(x)
+    N = x.shape[1]
+    mmean = msum/N
+    diff = np.square(x - mmean)
+    desv = math.sqrt(np.sum(diff)/N)
+    #print mmean, desv
+    x = x - mmean
+    x = np.divide(x, desv)
+    return x
+
 def softMax(z):
     nume = np.exp(z)
     denom = np.sum(nume)
     ret = nume / denom
     return ret
 
+# Neural network class ---------------------------------------------------------
 class nn:
     def __init__(self, d, M, K, b = 'sig', p = 'clas'):
-        self.dim = d + 1
+        self.dim = d + 1 # input
         self.neurons = M
         self.outs = K
         self.w1 = np.transpose(np.matrix(np.random.standard_normal((d+1)*M)))
@@ -62,13 +74,28 @@ class nn:
             ret = np.divide((np.exp(A) - np.exp(-A)),(np.exp(A) + np.exp(-A)))
         return ret
 
+    def cost_fun_eval(self, xx, T):
+        if(self.tp == 'clas'):
+            if(self.outs == 1):
+                Y = self.forwardPropagation(xx)
+                tmp1 = np.multiply(np.log(Y), T)
+                tmp2 = np.multiply(np.log(1.0 - Y), (1.0 - T))
+                ret = tmp1 + tmp2
+                ret = np.sum(ret)
+                return -ret
+
     def computeDelta2(self, Y, T):
         return Y - T
 
     def computeDelta1(self, Y, T):
         tmp = T - Y
+        if(self.tp == 'clas' and self.outs == 1):
+            tmp2 = np.multiply(Y, 1.0 - Y)
+            tmp = np.divide(tmp, tmp2)
+            tmp = -tmp
         #print tmp.shape
         #print self.wmat2.shape
+        #print np.transpose(tmp).shape, self.wmat2.shape
         tmp = np.transpose(tmp)*self.wmat2
         h_der = 0
         #print tmp.shape
@@ -91,14 +118,20 @@ class nn:
         Z = self.basFunEval(A)
         self.zi = Z
         Y = self.activ2Eval(Z)
-        if(self.tp == 'clas' and self.outs == 1):
+        #print Y
+        if(self.tp == 'clas'):
             Y = 1.0 / (1.0 + np.exp(-Y))
+        elif(self.tp == 'mult_clas'):
+            for i in range(0, Y.shape[1]):
+                Y[:,i] = softMax(Y[:,i])
+        #print Y
         return Y
 
     def errorGradient(self, xx, Y, T):
         delta_2 = self.computeDelta2(Y,T)
         derivative2 = delta_2*np.transpose(self.zi)
         delta_1 = self.computeDelta1(Y,T)
+        #print delta_1.shape, xx.shape
         derivative1 = delta_1*np.transpose(xx)
         l = 0
         r = self.neurons
@@ -112,46 +145,47 @@ class nn:
             self.gradw2[l:r,0] = derivative2[:,i]
             l = r
             r = r + self.outs
-
+        #print np.concatenate((self.gradw1, self.gradw2), axis = 0)
         return np.concatenate((self.gradw1, self.gradw2), axis = 0)
 
     def process(self, xx, T):
+        try:
+            if(self.outs == T.shape[0]):
+                1
+            else:
+                raise Exception
+        except Exception as inst:
+            print "Holy Shit!!!, the dimensions between real and computed outputs are not equal"
+            return
         cont = 0
         Y = 0
         while(cont < 15):
+            #print self.cost_fun_eval(xx, T)
             Y = self.forwardPropagation(xx)
             gradient_w = self.errorGradient(xx, Y, T)
             w_new = self.W - (0.1*gradient_w)
+            diff = np.sqrt(np.sum(np.square(w_new)))
             self.W = w_new
             self.w1 = w_new[0:self.dim*self.neurons, 0]
             self.w2 = w_new[self.dim*self.neurons:w_new.shape[0], 0]
-            #print Y
             cont = cont + 1
-        print Y
+
+        print np.transpose(Y)
 
     def debug(self):
         print self.w2
         print self.wmat2
 
-'''
-X = np.random.standard_normal(size = (2,6))*10.
-T = np.random.binomial(1, 0.5, 6)
-tmp = np.ones(X.shape[1])
-mneural = nn(X.shape[0], 5, 1)
-XX = np.zeros((X.shape[0] + 1, X.shape[1]))
-XX[1:,:] = X
-XX[0,:] = tmp
-XX = np.matrix(XX)
-print XX, T
-mneural.process(XX, T
-'''
 
 mdata = scipy.io.loadmat('ejemplo_class_uno.mat')
 X = np.array(mdata['X'])
 t = np.array(mdata['t'])
 for i in range(0, t.shape[0]):
     if(t[i,0] == -1):
-        t[i,0] = 0
+        t[i,0] = 0.0
+    else:
+        t[i,0] = 1.0
+
 t = np.matrix(t)
 t = np.transpose(t)
 tmp = np.ones(X.shape[0])
@@ -160,25 +194,73 @@ XX[:,1:] = X
 XX[:,0] = tmp
 XX = np.matrix(XX)
 XX = np.transpose(XX)
+print XX, t
+#print np.amax(XX[1,:]), np.amin(XX[1,:])
+#print np.amax(XX[2,:]), np.amin(XX[2,:])
+#print ' '
+#print np.transpose(XX), np.transpose(t)
 mneural = nn(XX.shape[0] - 1, 2, 1, 'tanh')
-#print XX, t
+#print ' '
 mneural.process(XX, t)
-
-
+#print XX, t
+XX = np.transpose(XX)
+t = np.transpose(t)
 
 '''
-out = np.zeros(1000)
-for i in range(0,len(out)):
-    out[i] = Y[0,i]
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot_trisurf(X[0,:], X[1,:], out, cmap=cm.jet, linewidth=0.2, vmin = 0.0, vmax = 1.0)
+plt.figure(1)
+plt.plot(XX[0:49,1], XX[0:49,2], 'bo')
+plt.plot(XX[50:100,1], XX[50:100,2], 'rx')
 plt.show()
+'''
 
+'''
+file = open('iris.data', 'r')
+table = [row.strip().split(',') for row in file]
+data = np.matrix(table)
+label = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
+label_biclass_1 = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 1}
 
-#mneural.debug()
-#mneural.weight2ToMat()
-#print ' '
-#mneural.debug()
+def assignVar(N):
+    a = np.zeros((N,1))
+    b = np.zeros((N,1))
+    c = np.zeros((N,1))
+    d = np.zeros((N,1))
+    e = np.zeros((N,3))
+    for i in range(0, N):
+        tmp = data[:,0]
+        a[i,0] = float(tmp[i,0])
+        tmp = data[:,1]
+        b[i,0] = float(tmp[i,0])
+        tmp = data[:,2]
+        c[i,0] = float(tmp[i,0])
+        tmp = data[:,3]
+        d[i,0] = float(tmp[i,0])
+        tmp = data[:,4]
+        e[i, label[tmp[i,0]]] = 1.
+    unos=np.ones((N,1))
+    XX = np.concatenate((unos,a,b,c,d),1)
+    return np.matrix(XX), np.matrix(e)
+
+XX, T = assignVar(len(data[:,1]))
+XX = np.transpose(XX)
+T = np.transpose(T)
+T = T[0,:]
+X = XX[0:3,:]
+X[2,:] = XX[3,:]
+# Normalization
+X[1,:] = normalize_arr_gauss(X[1,:])
+X[2,:] = normalize_arr_gauss(X[2,:])
+#print X,T
+#print np.amax(X[1,:]), np.amin(X[1,:])
+#print np.amax(X[2,:]), np.amin(X[2,:])
+
+mneural = nn(X.shape[0] - 1, 2, 1, 'tanh')
+mneural.process(X, T)
+
+X = np.transpose(X)
+T = np.transpose(T)
+plt.figure(2)
+plt.plot(X[0:49,1], X[0:49,2], 'bo')
+plt.plot(X[50:150,1], X[50:150,2], 'rx')
+plt.show()
 '''
